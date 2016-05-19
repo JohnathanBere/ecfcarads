4cfb868630c6b0b3c82477ca0e2846c1c7beab53
JohnathanBere <johnbere@live.co.uk>
2016-05-19 22:00:57 +0100

    First time commit

---
+++ adCars/Controllers/AdsController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using adCars.Models;
+using adCars.ViewModels;
+
+namespace adCars.Controllers
+{
+    public class AdsController : Controller
+    {
+        /* public ActionResult Index()
+        {
+            var ad = new Ad() {CarMake = "Bugati"};
+            var customers = new List<Customer>()
+            {
+                new Customer {Name = "Phillis"},
+                new Customer {Name = "Regis"}
+            };
+
+            var viewModel = new AdsViewModel
+            {
+                Ad = ad,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        } */
+
+        public ViewResult Index()
+        {
+            var ads = GetAds();
+            return View(ads);
+        }
+
+        private IEnumerable<Ad> GetAds()
+        {
+            return new List<Ad>
+            {
+                new Ad {CarMake = "Bugati"},
+                new Ad {CarMake = "Lamborghini"}
+            };
+        }
+
+
+    }
+}
---
+++ adCars/ViewModels/AdsViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using adCars.Models;
+
+namespace adCars.ViewModels
+{
+    public class AdsViewModel
+    {
+        public Ad Ad { get; set; }
+        public List<Customer> Customers { get; set; }
+    }
+}
